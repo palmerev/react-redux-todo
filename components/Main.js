6e8606f18e08fd13406cdff6f9c18c830ce3112e
Evan Palmer <evjpalmer@gmail.com>
2016-04-14 14:05:28 -0700

    initial project skeleton

---
+++ components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default class Main extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+  render() {
+    return (
+      <div>This is the main component</div>
+    )
+  }
+}
